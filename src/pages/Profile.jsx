97110bd73ff2bb3700d6fa392aabb894da8a47d1
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-04-30 22:58:39 +0530

    profile page fix 2

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,13 +4,13 @@ export default function Profile() {
   return (
     <>
       <div className="flex-col">
-        <div className="flex justify-around gap-20   items-center">
+        <div className="flex justify-around gap-10   items-center">
           <div className="flex-col p-4 mt-3">
             <h2 className="text-2xl font-semibold">Alen Walker</h2>
             <h2 className="text-2xl font-semibold">8093483115</h2>
             <h2 className="text-2xl font-semibold">test@gmail.com</h2>
           </div>
-          <h2 className="w-2/4 bg-[#2827CC] p-4 m-4 rounded-full text-4xl text-center text-white ">P</h2>
+          <h2 className="w-1/4 bg-[#2827CC] p-4 m-4 rounded-full text-4xl text-center text-white ">P</h2>
         </div>
 
         <button>Edit</button>
