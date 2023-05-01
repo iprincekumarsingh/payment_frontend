629b443b9feab769d85fb565a019644385d3e624
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-05-01 11:30:52 +0530

    Improved login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import toast, { Toaster } from "react-hot-toast";
 import axios from "./api/axios";
 import Cookie from "js-cookie";
 
+import logo from "./img/sxbank.jpg";
+
 function App() {
   useEffect(() => {
     document.title = "Phone Login";
@@ -10,7 +12,9 @@ function App() {
     if (Cookie.get("token")) {
       window.location.href = "/home";
     }
-  });
+
+  }
+  );
 
   const [count, setCount] = useState(0);
 
@@ -34,26 +38,27 @@ function App() {
 
     setPhone_number("91" + phone);
 
-    axios
-      .post("/auth/login", { phone_number: phone })
-      .then((res) => {
-        console.log(res.data);
-        if (res.data.success === true) {
-          toast.success("OTP sent successfully");
-          const randomq = Math.floor(100000 + Math.random() * 900000);
-          window.location.href =
-            "/auth/login/verify?phone=" + phone + "&rand=" + randomq;
-          // console.log(res.data);
 
-          // how to mount the otp component
-        } else {
-          toast.error("OTP sent failed");
-        }
-      })
+    axios.post("/auth/login", { phone_number: phone }).then((res) => {
+      console.log(res.data);
+      if (res.data.success === true) {
+        toast.success("OTP sent successfully");
+        const randomq = Math.floor(100000 + Math.random() * 900000);
+        window.location.href = "/auth/login/verify?phone=" + phone + "&rand=" + randomq;
+        // console.log(res.data);
+
+        // how to mount the otp component
+      } else {
+        toast.error("OTP sent failed");
+
+      }
+    })
       .catch((err) => {
         console.log(err);
         toast.error("Something went wrong");
-      });
+      }
+      );
+
   };
 
   return (
@@ -61,12 +66,18 @@ function App() {
       {/* component */}
       <Toaster />
       <div className="relative flex min-h-screen flex-col justify-center overflow-hidden  py-12">
-        <div className="relative  px-6 pt-10 pb-9 shadow-xl mx-auto w-full max-w-lg rounded-2xl">
-          <div className="mx-auto flex w-full max-w-md flex-col space-y-16">
+        <div className="relative  px-6 pt-10 pb-9  mx-auto w-full max-w-lg rounded-2xl">
+          <div className="mx-auto flex w-full max-w-md flex-col space-y-16 p-3 rounded-lg" style={
+            {
+              border:"1px solid rgb(37, 150, 190)"
+            }
+          }>
             <div className="flex flex-col items-center justify-center text-center space-y-2">
-              <div className="font-semibold text-3xl">
-                <p>Phone Login</p>
+              <div className="font-semibold text-3xl flex-col justify-center items-center">
+
+                <img width={50} src={logo} alt="" />
               </div>
+              <p className="text-3xl">Sign In To Your Account</p>
             </div>
             <div>
               <form
@@ -77,6 +88,7 @@ function App() {
                 <div className="flex flex-col space-y-16">
                   <div className="flex flex-row items-center justify-between mx-auto w-full max-w-xs">
                     <div className="w-full h-16 ">
+                      <label htmlFor="" className="p-2 text-[16px]" >Phone Number </label>
                       <input
                         className="w-full h-full flex flex-col items-center justify-center text-start px-5 outline-none rounded-xl border border-gray-200 text-lg bg-white focus:bg-gray-50 focus:ring-1 ring-blue-700"
                         type="text"
@@ -95,6 +107,7 @@ function App() {
                         Login
                       </button>
                     </div>
+                    <a className="text-[12px] text-center text-blue-600" href="">By Login you accept the TERMS & CONDITIONS </a>
                   </div>
                 </div>
               </form>
