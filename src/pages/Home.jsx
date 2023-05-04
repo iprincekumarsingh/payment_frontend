fcb4aa8f142cb7a1ef5378f3f1d77a9053d956ba
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-05-04 11:34:34 +0530

    fix3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -195,160 +195,164 @@ export default function Home() {
   };
 
   return (
-    <>
-      <div className="flex-col mb-4 ml-4 p-1 mt-6">
-        <h1 className="text-2xl  ">Welcome Back </h1>
-        <span className="text-3xl text-black font-bold ">{name}</span>
-        <div className="mt-2">Account no - {wallet}</div>
+    < >
 
-        {/* <h1 className="text-base">Account no - {localStorage.getItem}</h1> */}
-      </div>
-      <div>
-        <section className="container  mt-10 border-[black]-100 h-3/4">
-          <div className="  ">
-            <div className="flex justify-around items-center m-4 gap-4 rounded-md shadow-2xl shadow-black-500/40 p-10 h-2/4 ">
-              <div
-                onClick={openModal}
-                className="flex-col  justify-center items-center text-center  w-[100px] "
-                style={{
-                  background: "rgb(202, 213, 226)",
-                  padding: "10px",
-                  borderRadius: "10px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <img className="vertical-0" src={add_money} alt="" width={50} />
-                <p>Add Money</p>
-              </div>
+      <div className="overflow-x-auto">
 
-              <div
-                onClick={requestMoneyModal}
-                className="flex-col  justify-center items-center text-center w-[100px] "
-                style={{
-                  background: "rgb(202, 213, 226)",
-                  padding: "10px",
-                  borderRadius: "10px",
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  className="vertical-0"
-                  src={receive_money}
-                  alt=""
-                  width={50}
-                />
-                <p>Request Money</p>
+        <div className="flex-col mb-4 ml-4 p-1 mt-6">
+          <h1 className="text-2xl  ">Welcome Back </h1>
+          <span className="text-3xl text-black font-bold ">{name}</span>
+          <div className="mt-2">Account no - {wallet}</div>
+
+          {/* <h1 className="text-base">Account no - {localStorage.getItem}</h1> */}
+        </div>
+        <div>
+          <section className="container  mt-10 border-[black]-100 h-3/4">
+            <div className="  ">
+              <div className="flex justify-around items-center m-4 gap-4 rounded-md shadow-2xl shadow-black-500/40 p-10 h-2/4 ">
+                <div
+                  onClick={openModal}
+                  className="flex-col  justify-center items-center text-center  w-[100px] "
+                  style={{
+                    background: "rgb(202, 213, 226)",
+                    padding: "10px",
+                    borderRadius: "10px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <img className="vertical-0" src={add_money} alt="" width={50} />
+                  <p>Add Money</p>
+                </div>
+
+                <div
+                  onClick={requestMoneyModal}
+                  className="flex-col  justify-center items-center text-center w-[100px] "
+                  style={{
+                    background: "rgb(202, 213, 226)",
+                    padding: "10px",
+                    borderRadius: "10px",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                  <img
+                    className="vertical-0"
+                    src={receive_money}
+                    alt=""
+                    width={50}
+                  />
+                  <p>Request Money</p>
+                </div>
               </div>
             </div>
-          </div>
-          <Modal
-            isOpen={modalIsOpen}
-            onAfterOpen={afterOpenModal}
-            onRequestClose={closeModal}
-            style={customStyles}
-            contentLabel="Example Modal"
-          >
-            <section className="rounded-1xl p-4 border-[1px] border-black border-solid">
-              <div className="flex-col justify-center items-center ">
-                <hr />
-                <h1 className="text-3xl font-bold text-center">Send Money</h1>
-                <img
-                  className="justify-center items-center"
-                  src={qrcode}
-                  alt=""
-                  srcset=""
-                />
-                <p className="p-2 text-start mr-4 ml-4 text-red-500">
-                  send money on QR and Share the ss on Whatsapp no
-                </p>
-              </div>
-              <hr />
-            </section>
-          </Modal>
-          <Modal
-            isOpen={requestMoney}
-            onAfterOpen={afterOpenModal}
-            onRequestClose={closeRequestMoneyModal}
-            style={customStyles}
-            contentLabel="Example Modal"
-          >
-            <section className="rounded-2xl p-4 border-[0.5px] border-black border-solid">
-              <div className="flex-col justify-center items-center ">
-                <h1 className="text-1xl font-bold text-start text-blue-500">
-                  Available balance - 3000{" "}
-                </h1>
-              </div>
-
-              <form
-                onSubmit={(e) => {
-                  requesMoneyFunction(e);
-                }}
-              >
-                <div class="mb-4 mt-10">
-                  <label
-                    class="block text-gray-700 text-sm font-bold mb-2"
-                    for="username"
-                  ></label>
-                  <input
-                    class=" appearance-none border-solid    w-full py-5 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
-                    id="username"
-                    type="number"
-                    placeholder="Enter Amount to withdraw"
-                    onChange={(e) => setAmount(e.target.value)}
+            <Modal
+              isOpen={modalIsOpen}
+              onAfterOpen={afterOpenModal}
+              onRequestClose={closeModal}
+              style={customStyles}
+              contentLabel="Example Modal"
+            >
+              <section className="rounded-1xl p-4 border-[1px] border-black border-solid">
+                <div className="flex-col justify-center items-center ">
+                  <hr />
+                  <h1 className="text-3xl font-bold text-center">Send Money</h1>
+                  <img
+                    className="justify-center items-center"
+                    src={qrcode}
+                    alt=""
+                    srcset=""
                   />
-                  <p className="text-[#ff0000] mt-3 text-center">{message}</p>
-                  <p className="text-green-700 mt-3 text-center">{success}</p>
-                </div>
-                <button
-                  class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  type="submit"
-                >
-                  Request
-                </button>
-              </form>
-              <hr />
-            </section>
-          </Modal>
-        </section>
-      </div>
-      {wallet == undefined || wallet == "" ? (
-        ""
-      ) : (
-
-      <div>
-        <div className="shadow p-2">
-          <h2 className="text-2xl p-4 font-semibold">Card</h2>
-          <div className="w-[99%] p-1 h-56  bg-red-100 rounded-xl  text-white shadow-2xl transition-transform transform ">
-            <img
-              className="relative object-cover w-full h-full rounded-xl"
-              src="https://i.imgur.com/kGkSg1v.png"
-            />
-            <div className="w-full px-8 absolute top-8">
-              <div className="flex justify-between items-center">
-                <div className>
-                  <p className="font-medium">Debit Card</p>
-                  <p className="font-bold">
-                    {split_wallet}
+                  <p className="p-2 text-start mr-4 ml-4 text-red-500">
+                    send money on QR and Share the ss on Whatsapp no
                   </p>
                 </div>
-                <img className="w-14 h-14" src={logo} />
-              </div>
-              <div className="pt-1 flex justify-start ">
-                {/* <p className="text-sm font-medium">Valid Thru</p> */}
-              </div>
-              <p className="text-2xl font-medium">{name}</p>
-              <div className="pt-1  flex justify-end  ">
-                <img className="w-14 h-14" src={logo2} />
+                <hr />
+              </section>
+            </Modal>
+            <Modal
+              isOpen={requestMoney}
+              onAfterOpen={afterOpenModal}
+              onRequestClose={closeRequestMoneyModal}
+              style={customStyles}
+              contentLabel="Example Modal"
+            >
+              <section className="rounded-2xl p-4 border-[0.5px] border-black border-solid">
+                <div className="flex-col justify-center items-center ">
+                  <h1 className="text-1xl font-bold text-start text-blue-500">
+                    Available balance - 3000{" "}
+                  </h1>
+                </div>
+
+                <form
+                  onSubmit={(e) => {
+                    requesMoneyFunction(e);
+                  }}
+                >
+                  <div class="mb-4 mt-10">
+                    <label
+                      class="block text-gray-700 text-sm font-bold mb-2"
+                      for="username"
+                    ></label>
+                    <input
+                      class=" appearance-none border-solid    w-full py-5 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+                      id="username"
+                      type="number"
+                      placeholder="Enter Amount to withdraw"
+                      onChange={(e) => setAmount(e.target.value)}
+                    />
+                    <p className="text-[#ff0000] mt-3 text-center">{message}</p>
+                    <p className="text-green-700 mt-3 text-center">{success}</p>
+                  </div>
+                  <button
+                    class="bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    type="submit"
+                  >
+                    Request
+                  </button>
+                </form>
+                <hr />
+              </section>
+            </Modal>
+          </section>
+        </div>
+        {wallet == undefined || wallet == "" ? (
+          ""
+        ) : (
+
+       
+            <div className="shadow p-2 mb-20">
+              <h2 className="text-2xl p-4 font-semibold">Card</h2>
+              <div className="w-[99%] p-1 h-56  bg-red-100 rounded-xl  text-white shadow-2xl transition-transform transform ">
+                <img
+                  className="relative object-cover w-full h-full rounded-xl"
+                  src="https://i.imgur.com/kGkSg1v.png"
+                />
+                <div className="w-full px-8 absolute top-8">
+                  <div className="flex justify-between items-center">
+                    <div className>
+                      <p className="font-medium">Debit Card</p>
+                      <p className="font-bold">
+                        {split_wallet}
+                      </p>
+                    </div>
+                    <img className="w-14 h-14" src={logo} />
+                  </div>
+                  <div className="pt-1 flex justify-start ">
+                    {/* <p className="text-sm font-medium">Valid Thru</p> */}
+                  </div>
+                  <p className="text-2xl font-medium">{name}</p>
+                  <div className="pt-1  flex justify-end  ">
+                    <img className="w-14 h-14" src={logo2} />
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+         
+        )}
       </div>
-      )}
     </>
   );
 }
