26ca3f61852b825e86fea5e0983986fed4949b55
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-05-07 14:11:13 +0530

    modal redesign

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,6 +17,8 @@ import HomeIcons from "../components/HomeIcons";
 import Widget_card from "../components/Widget_card";
 
 export default function Home() {
+  Modal.setAppElement("#root");
+
   const customStyles = {
     content: {
       top: "50%",
@@ -24,12 +26,12 @@ export default function Home() {
       right: "auto",
       bottom: "auto",
       marginRight: "-50%",
-      width: "90%",
-      padding: "10px",
-      shadow: "black",
-      background: "white",
-      border: "none",
       transform: "translate(-50%, -50%)",
+      width: "90%",
+      maxWidth: "500px",
+    },
+    overlay: {
+      backgroundColor: "rgba(0,0,0,0.5)",
     },
   };
 
@@ -235,57 +237,95 @@ export default function Home() {
               isOpen={modalIsOpen}
               onAfterOpen={afterOpenModal}
               onRequestClose={closeModal}
-              style={customStyles}
+              style={{
+                overlay: {
+                  backgroundColor: "rgba(0, 0, 0, 0.6)",
+                  zIndex: "50",
+                },
+                content: {
+                  top: "50%",
+                  left: "50%",
+                  right: "auto",
+                  bottom: "auto",
+                  // marginRight: "-50%",
+                  transform: "translate(-50%, -50%)",
+                  borderRadius: "1rem",
+                  border: "1px solid rgba(189, 189, 189, 1)",
+                  boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+                  // padding: "2rem",
+                  minWidth: "20rem",
+                  maxWidth: "80vw",
+                },
+              }}
+              // contentLabel="Request Money Modal"
               contentLabel="Example Modal"
-              W
             >
-              <section className="rounded-1xl p-4 border-[1px] border-black border-solid">
-                <div className="flex-col justify-center items-center ">
-                  <hr />
-                  <h1 className="text-3xl font-bold text-center">Send Money</h1>
+              <section className="rounded-2xl p-4 border border-gray-300">
+                <div className="flex flex-col items-center">
+                  <h1 className="text-3xl font-bold text-center mb-4">
+                    Send Money
+                  </h1>
                   <img
-                    className="justify-center items-center"
+                    className="w-full max-w-sm mb-4"
                     src={qrcode}
-                    alt=""
-                    srcset=""
+                    alt="QR code"
                   />
-                  <p className="p-2 text-start mr-4 ml-4 text-red-500">
-                    send money on QR and Share the ss on Whatsapp no
+                  <p className="text-sm text-gray-500 mb-4">
+                    Scan the QR code to send money and share the screenshot on
+                    WhatsApp.
                   </p>
+                  <button
+                    className="bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    onClick={closeModal}
+                  >
+                    Close
+                  </button>
                 </div>
-                <hr />
               </section>
             </Modal>
+
             <Modal
               isOpen={requestMoney}
               onAfterOpen={afterOpenModal}
               onRequestClose={closeRequestMoneyModal}
-              style={customStyles}
-              contentLabel="Example Modal"
+              style={{
+                overlay: {
+                  backgroundColor: "rgba(0, 0, 0, 0.6)",
+                  zIndex: "50",
+                },
+                content: {
+                  top: "50%",
+                  left: "50%",
+                  right: "auto",
+                  bottom: "auto",
+                  marginRight: "-50%",
+                  transform: "translate(-50%, -50%)",
+                  borderRadius: "1rem",
+                  border: "1px solid rgba(189, 189, 189, 1)",
+                  boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+                  padding: "2rem",
+                  minWidth: "20rem",
+                  maxWidth: "80vw",
+                },
+              }}
+              contentLabel="Request Money Modal"
             >
-              <section
-                className="rounded-2xl p-4 "
-                style={{ border: "1px solid rgba(189, 189, 189, 1)" }}
-              >
-                <div className="flex-col justify-center items-center ">
-                  <h1 className="text-1xl font-bold text-start text-blue-500">
+              <section className="border ">
+                <div className="flex-col justify-center items-center">
+                  <h1 className="text-xl font-bold text-start text-blue-500">
                     Available balance - 3000{" "}
                   </h1>
                 </div>
 
-                <form
-                  onSubmit={(e) => {
-                    requesMoneyFunction(e);
-                  }}
-                >
-                  <div class="mb-1 mt-4">
+                <form onSubmit={requesMoneyFunction}>
+                  <div className="mb-1 mt-4">
                     <h1>
                       Request Money to{" "}
                       <span className="font-bold"> SX Bank</span>
                     </h1>
                     <label
-                      class="block text-gray-700 text-sm font-bold mb-2"
-                      for="username"
+                      className="block text-gray-700 text-sm font-bold mb-2"
+                      htmlFor="username"
                     ></label>
                     <input
                       style={{
@@ -294,68 +334,40 @@ export default function Home() {
                         marginBottom: "10px",
                         borderRadius: "10px",
                       }}
-                      value={amount}
-                      class=" appearance-none border-solid    w-full py-5 px-3 text-black leading-tight focus:outline-none focus:shadow-outline"
+                      className="w-full py-5 px-3 text-black leading-tight focus:outline-none focus:shadow-outline appearance-none"
                       id="username"
                       type="number"
                       placeholder="Enter Amount to withdraw"
+                      value={amount}
                       onChange={(e) => setAmount(e.target.value)}
                     />
                     <div className="flex justify-even overflow-x-auto w-full text-center ">
                       <div
-                        className="flex "
-                        style={{
-                          border: "1px solid rgba(189, 189, 189, 1)",
-                          padding: "8px",
-                          margin: "5px",
-                          fontSize: "18px",
-                          borderRadius: "10px",
-                        }}
+                        className="flex border border-solid border-gray-400 rounded-lg p-2 m-2 text-lg cursor-pointer"
                         onClick={() => setAmount(Number(amount) + Number(100))}
                       >
                         ₹100
                       </div>
                       <div
-                        className="flex "
-                        style={{
-                          border: "1px solid rgba(189, 189, 189, 1)",
-                          padding: "8px",
-                          margin: "5px",
-                          fontSize: "18px",
-                          borderRadius: "10px",
-                        }}
+                        className="flex border border-solid border-gray-400 rounded-lg p-2 m-2 text-lg cursor-pointer"
                         onClick={() => setAmount(Number(amount) + Number(200))}
                       >
                         ₹200
                       </div>
                       <div
-                        className="flex "
-                        style={{
-                          border: "1px solid rgba(189, 189, 189, 1)",
-                          padding: "8px",
-                          margin: "5px",
-                          fontSize: "18px",
-                          borderRadius: "10px",
-                        }}
+                        className="flex border border-solid border-gray-400 rounded-lg p-2 m-2 text-lg cursor-pointer"
                         onClick={() => setAmount(Number(amount) + Number(1000))}
                       >
                         ₹1000
                       </div>
                       <div
-                        className="flex "
-                        style={{
-                          border: "1px solid rgba(189, 189, 189, 1)",
-                          padding: "8px",
-                          margin: "5px",
-                          fontSize: "18px",
-                          borderRadius: "10px",
-                        }}
+                        className="flex border border-solid border-gray-400 rounded-lg p-2 m-2 text-lg cursor-pointer"
                         onClick={() => setAmount(Number(amount) + Number(2000))}
                       >
                         ₹2000
                       </div>
                     </div>
-                    <p className="text-[#ff0000] mt-3 text-center">{message}</p>
+                    <p className="text-red-500 mt-3 text-center">{message}</p>
                     <p className="text-green-700 mt-3 text-center">{success}</p>
                   </div>
                   <button
@@ -370,7 +382,6 @@ export default function Home() {
             </Modal>
           </section>
         </div>
-
       </div>
     </div>
   );
