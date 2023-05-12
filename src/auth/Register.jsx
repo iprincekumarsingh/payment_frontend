880f5965b6ca3f9295daf33dc5bbc91a5926885f
Prince Kumar Singh <princekumar2000.pks@gmail.com>
2023-05-12 12:37:52 +0530

    Added signup logo

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -5,6 +5,7 @@ import BeatLoader from "react-spinners/BeatLoader";
 import Cookie from "js-cookie";
 import toast, { Toaster } from "react-hot-toast";
 import axios from "../api/axios";
+import logo from "../img/sxbank.jpg";  
 const override = {
   display: "block",
   margin: "0 auto",
@@ -97,6 +98,9 @@ export default function Register() {
       <Toaster />
       <div className="flex items-center justify-center px-4 py-10 sm:px-6 sm:py-16 lg:px-8 lg:py-24">
         <div className="xl:mx-auto xl:w-full xl:max-w-sm 2xl:max-w-md">
+        <div className="flex justify-center p-4 items-center">
+             <img src={logo} width={100}  className="flex justify-center items-center rounded-xl" alt="" srcset={logo} />
+             </div>
           <h2 className="text-3xl font-bold leading-tight text-black  sm:text-4xl">
             Sign Up
           </h2>
@@ -203,7 +207,10 @@ export default function Register() {
                   {" "}
                   terms of service
                 </span>
-                to learn more
+                to <span style={{
+
+                  lineHeight:"1px"
+                }}>  learn more</span>
               </span>
             </p>
           </div>
