8f8bff984af59783456692e88d83b5c90ce843b2
princekumar <princekumar2000.pks@gmail.com>
2023-06-29 21:58:58 +0530

    Fix 2

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
   // baseURL: "https://vivacious-helmet-newt.cyclic.app/api/v1",
-  baseURL: "https://wild-blue-bunny-cuff.cyclic.app/api/v1",
+  baseURL: "https://puzzled-beanie-goat.cyclic.app/api/v1",
   // baseURL: "http://127.0.0.1:4000/api/v1",
 });
