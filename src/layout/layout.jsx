4958e2b5b8997e9093fb66e4137634eeb5c04c0c
princekumar <princekumar2000.pks@gmail.com>
2023-07-29 00:39:58 +0530

    fix 2

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -11,35 +11,15 @@ import LocationOnIcon from "@mui/icons-material/LocationOn";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import { useNavigate } from "react-router-dom";
 export default function LayoutHome() {
   const [value, setValue] = React.useState(0);
+  const navigate = useNavigate();
   return (
     <>
+    
       <Outlet />
-      <Box sx={{ width: "100vw", position: "absolute", bottom: 0 }}>
-        <BottomNavigation
-          showLabels
-          value={value}
-          onChange={(event, newValue) => {
-            setValue(newValue);
-          }}
-        >
-          <BottomNavigationAction
-            label="Home"
-            icon={
-              <HomeOutlinedIcon
-                onClick={() => navigate("../../home/home/user")}
-              />
-            }
-          />
-          <BottomNavigationAction label="Profile" icon={<AccountCircle />} />
-          <BottomNavigationAction
-            label="Setting"
-            icon={<SettingsOutlinedIcon />}
-          />
-        </BottomNavigation>
-      </Box>
-      )
     </>
+    
   );
 }
